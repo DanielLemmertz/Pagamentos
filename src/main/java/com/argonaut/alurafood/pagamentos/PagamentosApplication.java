4ddf93ec1516874da3f3f59582acd1bf3eb14fab
DanielLemmertz <danielmultimate5178@gmail.com>
2025-04-28 20:27:58 -0300

    Starting Project

---
+++ src/main/java/com/argonaut/alurafood/pagamentos/PagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.argonaut.alurafood.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosApplication.class, args);
+	}
+
+}
